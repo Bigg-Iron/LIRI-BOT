e41761bb83c3be34378f8129f44bca6e085c8694
Lorenzo <lorenzo.bartolo@gmail.com>
2017-10-07 08:57:41 -0700

    Added Twitter API

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'A1n9ZcljEk0gF2ZIMX5JAKAx8',
+  consumer_secret: 'nGCcAUihEpDWaL1EJBIJIRrQY5lx33kHs2WfG30PW4iXjSABJN',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+module.exports = twitterKeys;
